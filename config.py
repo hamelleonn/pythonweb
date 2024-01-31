648ac86f1abe749e1ccdeab3134e1b30bc5acb99
hamelleonn <57328755+hamelleonn@users.noreply.github.com>
2024-01-31 09:47:02 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+from os import environ
+
+APP_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+class Config(object):
+    FLASK_DEBUG = False
+    DEVELOPMENT = False
+    SECRET_KEY = "asdasd"
+    FLASK_SECRET = SECRET_KEY
+
+    # Login manager
+    LOGIN_MANAGER_LOGIN_VIEW = "auth.login"
+    LOGIN_MANAGER_LOGIN_MESSAGE_CATEGORY = "info"
+
+    # Images
+    UPLOAD_FOLDER = 'app/static/images'
+    ALLOWED_EXTENSIONS = {'jpg', 'png'}
+
+    # SQLALCHEMY
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///feedbacks.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def get_config():
+        return config[environ.get('CONFIG') or "DEFAULT"]
+
+
+class DevConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class ProdConfig(Config):
+    DEVELOPMENT = False
+    DEBUG = False
+
+
+config = {
+    'DEV': DevConfig,
+    'PROD': ProdConfig,
+    'DEFAULT': DevConfig,
+}
